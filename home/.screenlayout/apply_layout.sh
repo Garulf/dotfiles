edcc0bc12b77551d6b6edb7afc19858e18dbc938
Garulf <535299+Garulf@users.noreply.github.com>
2024-07-30 02:50:54 -0400

    Improve xrandr defaults

---
+++ home/.screenlayout/apply_layout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOSTNAME="$(hostname)"
+
+# detect if directory named after hostname exists
+
+echo "Looking for layout for $HOSTNAME"
+
+if [ -d "$HOME/.screenlayout/$HOSTNAME" ]; then
+  echo "Applying layout for $HOSTNAME"
+  sh "$HOME/.screenlayout/$HOSTNAME/default.sh"
+fi
