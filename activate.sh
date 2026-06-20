4342fee959f016be45c609ed9617a6ee894933ac
Garulf <535299+Garulf@users.noreply.github.com>
2026-06-20 10:37:53 -0400

    feat: Bootstrap TPM install in activate.sh

--- activate.sh
+++ activate.sh
@@ -11,3 +11,8 @@ git pull
 
 echo "${GREEN}Restowing dotfiles...${NC}"
 stow --restow home && echo "${GREEN}dotfiles are now active!${NC}"
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "${GREEN}Installing TPM...${NC}"
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
